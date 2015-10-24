d1b7a1454431dd75ff3d41f7ebd970ed06090c51
Alejandro <alejandro.sanchez.cabana@gmail.com>
2015-10-24 19:59:40 +0200

    user activities done

---
+++ service/src/main/java/edu/upc/eetac/dsa/grouptalk/GroupTalkMediaType.java
@@ -0,0 +1,14 @@
+package edu.upc.eetac.dsa.grouptalk;
+
+/**
+ * Created by Alex on 24/10/15.
+ */
+public class GroupTalkMediaType {
+    public final static String GROUPTALK_AUTH_TOKEN = "application/vnd.dsa.grouptalk.auth-token+json";
+    public final static String GROUPTALK_USER = "application/vnd.dsa.grouptalk.user+json";
+    public final static String GROUPTALK_GROUP = "application/vnd.dsa.grouptalk.group+json";
+    public final static String GROUPTALK_GROUP_COLLECTION = "application/vnd.dsa.grouptalk.group.collection+json";
+    public final static String GROUPTALK_COMMENT = "application/vnd.dsa.grouptalk.group.comment+json";
+    public final static String GROUPTALK_COMMENT_COLLECTION = "application/vnd.dsa.grouptalk.group.message.collection+json";
+    public final static String GROUPTALK_ROOT = "application/vnd.dsa.grouptalk.root+json";
+}
